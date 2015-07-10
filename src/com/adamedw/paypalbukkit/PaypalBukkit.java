5bae132405a704d224a6f2ca3a0f1ac47f7022e6
8rrico <8rrico@gmail.com>
2015-07-10 15:01:03 -0400

    Initial push

---
+++ src/com/adamedw/paypalbukkit/PaypalBukkit.java
@@ -0,0 +1,21 @@
+package com.adamedw.paypalbukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PaypalBukkit extends JavaPlugin {
+
+
+	@Override
+	public void onEnable()
+	{
+		
+	}
+
+	@Override
+	public void onDisable()
+	{
+		
+	}
+
+
+}
