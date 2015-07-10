db9c7325cf8345a024445e85f3e23743a1d2290f
8rrico <8rrico@gmail.com>
2015-07-10 15:18:44 -0400

    Prepare 4 teh codin!

--- src/com/adamedw/paypalbukkit/PaypalBukkit.java
+++ src/com/adamedw/paypalbukkit/PaypalBukkit.java
@@ -2,13 +2,15 @@ package com.adamedw.paypalbukkit;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.adamedw.paypalbukkit.command.Commandpaypal;
+
 public class PaypalBukkit extends JavaPlugin {
 
 
 	@Override
 	public void onEnable()
 	{
-		
+		this.getCommand("paypal").setExecutor(new Commandpaypal());
 	}
 
 	@Override
---
+++ src/com/adamedw/paypalbukkit/command/Commandpaypal.java
@@ -0,0 +1,18 @@
+package com.adamedw.paypalbukkit.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Commandpaypal implements CommandExecutor {
+
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+
+		return true;
+	}
+
+
+}
